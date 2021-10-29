76902363aa7edc806ea00fe612426352a0f86237
Narek-Avdalyan <nareka161@gmail.com>
2021-10-29 19:10:41 +0400

    correctstyle

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
-let square1 = "red";
-let square2 = "green";
-let square3 = "blue";
-let square4 = "gold";
-let arr = [square1,square2,square3,square4]
-function changecolors(){
+const changesquare1 = document.createElement("div");
+const changesquare2 = document.createElement("div");
+const changesquare3 = document.createElement("div");
+const changesquare4 = document.createElement("div");
 
-function randomArr(a,b) {
-    return Math.random() - 0.5;
+const arr = [changesquare1,changesquare2,changesquare3,changesquare4]
+
+randomArr = () => {
+    return Math.floor(Math.random() * 300);
 }
-console.log(arr);
-arr.sort(randomArr);
+
+changecolors = () => {
 document.getElementById("foot").innerHTML = "";
 const newdiv = document.createElement("div");
-newdiv.setAttribute("id", "firstdiv");
+newdiv.setAttribute("id", "logo");
 
-const changesquare1 =document.createElement("div");
-changesquare1.id = arr[0];
-
-const changesquare2 =document.createElement("div");
-changesquare2.setAttribute("id",arr[1]);
-
-const changesquare3 =document.createElement("div");
-changesquare3.setAttribute("id",arr[2]);
-
-const changesquare4 =document.createElement("div");
-changesquare4.setAttribute("id",arr[3]);
-
-newdiv.appendChild(changesquare1);
-newdiv.appendChild(changesquare2);
-newdiv.appendChild(changesquare3);
-newdiv.appendChild(changesquare4);
+arr.forEach(element => {
+    element.id = "colors";
+    element.setAttribute("style", `background-color: rgb(${randomArr() + ',' + randomArr() + ',' + randomArr()})`);
+    newdiv.appendChild(element);
+});
 
 document.getElementById("foot").appendChild(newdiv);
 }
